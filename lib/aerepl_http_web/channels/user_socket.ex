f389764b70a6c240f74c12bebc84c41157061fcf
radrow <radrowicki@gmail.com>
2022-08-13 14:00:19 +0200

    .

---
+++ lib/aerepl_http_web/channels/user_socket.ex
@@ -0,0 +1,12 @@
+defmodule AereplHttpWeb.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  channel "repl_session:*", AereplHttpWeb.ReplSessionChannel
+
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+end
