40cd021c000907277566e8bb4b03ae18d6726c0a
radrow <radrowicki@gmail.com>
2023-08-06 15:46:15 +0200

    Add session files

---
+++ lib/aerepl_http/session_data.ex
@@ -0,0 +1,22 @@
+defmodule AereplHttp.SessionData do
+  @moduledoc """
+  User session data.
+  """
+
+  defstruct id: nil,
+    start: nil,
+    last_interaction: nil
+
+  def new() do
+    struct(__MODULE__, [
+          id: UUID.uuid4(),
+          start: DateTime.utc_now(),
+          last_interaction: DateTime.utc_now(),
+        ])
+  end
+
+  def touch(session) do
+    %{session | last_interaction: DateTime.utc_now()}
+  end
+
+end
