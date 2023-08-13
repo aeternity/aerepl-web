06e79026a6d604302b774c27ef6e45702d29d724
radrow <radrowicki@gmail.com>
2023-08-13 16:29:13 +0200

    .

--- lib/aerepl_server/session_data.ex
+++ lib/aerepl_server/session_data.ex
@@ -4,14 +4,18 @@ defmodule AereplServer.SessionData do
   """
 
   defstruct id: nil,
+    client_id: nil,
     start: nil,
-    last_interaction: nil
+    last_interaction: nil,
+    timeout: nil,
 
-  def new() do
+  def new(client_id) do
     struct(__MODULE__, [
           id: UUID.uuid4(),
+          client_id: client_id,
           start: DateTime.utc_now(),
           last_interaction: DateTime.utc_now(),
+          timeout: Time.new(0, 0, 10)
         ])
   end
 
@@ -19,4 +23,10 @@ defmodule AereplServer.SessionData do
     %{session | last_interaction: DateTime.utc_now()}
   end
 
+  def is_timeout(session) do
+    timeout_sec = Time.to_seconds_after_midnight(session.timeout)
+    deadline = DateTime.add(session.last_interaction, timeout_sec, :second)
+    now = DateTime.utc_now()
+    DateTime.before?(deadline, now)
+  end
 end
--- lib/aerepl_server/session_service.ex
+++ lib/aerepl_server/session_service.ex
@@ -10,12 +10,15 @@ defmodule AereplServer.SessionService do
     {:via, Registry, {AereplServer.SessionRegistry, {__MODULE__, user_id}}}
   end
 
+
   def via_repl(%SessionData{id: session_id}) do
     via_repl(session_id)
   end
+
   def via_repl(id: session_id) when is_binary(session_id) do
     {:via, Registry, {AereplServer.ReplRegistry, {__MODULE__, session_id}}}
   end
+
 
   def child_spec(user_id) do
     %{
@@ -25,12 +28,14 @@ defmodule AereplServer.SessionService do
     }
   end
 
+
   def start(user_id) do
     DynamicSupervisor.start_child(
       AereplServer.SessionSupervisor,
       child_spec(user_id)
     )
   end
+
 
   def stop(user_id) do
     DynamicSupervisor.terminate_child(
@@ -39,12 +44,22 @@ defmodule AereplServer.SessionService do
     )
   end
 
+
   def terminate(reason, session) do
     GenServer.stop(repl_ref(session), reason)
   end
 
-  def init(session) do
-    {:ok, _repl} = :aere_gen_server.start_link(repl_ref(session), options: %{:filesystem => {:cached, %{}}})
+
+  def init(user_id) do
+    session = SessionData.new(user_id)
+
+    {:ok, _repl} = :aere_gen_server.start_link(
+      via_repl(user_id),
+      options: %{
+        :filesystem => {:cached, %{}}
+      }
+    )
+
     {:ok, session, {:continue, :init}}
   end
 
@@ -60,6 +75,7 @@ defmodule AereplServer.SessionService do
     input = String.to_charlist(text)
     output = :aere_gen_server.input(repl, input)
 
+    session = touch(session)
     case output do
       :no_output ->
         {:reply, "ok", session}
@@ -70,25 +86,67 @@ defmodule AereplServer.SessionService do
     end
   end
 
-
   def handle_call({:repl_load_files, filemap}, _from, session) do
     repl = repl_ref(session)
 
     :ok = :aere_gen_server.update_filesystem_cache(repl, filemap)
-    {:reply, "Updated file cache", session}
+    {:reply, "Updated file cache", touch(session)}
   end
-
 
   def handle_call(:banner, _from, session) do
     repl = repl_ref(session)
     banner = :aere_gen_server.banner(repl)
-    {:reply, List.to_string(banner), session}
+    {:reply, List.to_string(banner), touch(session)}
   end
 
+
+  def handle_cast(:timeout_check, session) do
+    if SessionData.is_timeout(session) do
+      {:stop, {:shutdown, :inactivity}, session}
+    else
+      {:noreply, session}
+    end
+  end
+
+
+  ### API
+
+  def repl_input_text(client_id, text) do
+    GenServer.call(via(client_id), {:repl_input_text, text})
+  end
+
+
+  def repl_load_files(client_id, filemap) do
+    GenServer.call(via(client_id), {:repl_load_files, filemap})
+  end
+
+
+  def repl_banner(client_id) do
+    GenServer.call(via(client_id), :repl_banner)
+  end
+
+
+  def repl_prompt(client_id) do
+    GenServer.call(via(client_id), :repl_prompt)
+  end
+
+
+  def timeout_check(client_id) do
+    GenServer.cast(via(client_id), :timeout_check)
+  end
+
+
+  ### Helpers
 
   def render(resp) do
     theme = :aere_theme.empty_theme()
 
     List.to_string(:aere_theme.render(theme, resp))
+  end
+
+
+  def touch(session) do
+    now = DateTime.utc_now()
+    spawn_link(fn() -> GenServer.cast(via(session.client_id)) end)
   end
 end
