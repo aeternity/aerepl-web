fdc97003d4f7f8644af02c121f89d4007b2c2cfc
Radosław Rowicki <35342116+radrow@users.noreply.github.com>
2024-01-09 10:31:20 +0100

    Revert "Genserver interface (#61)" (#62)
    This reverts commit ba9d2c6004fa3a810c76832bbf5aa58bcbb8cfb4.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -5,8 +5,10 @@ import {Socket} from "phoenix";
 var AU = require('ansi_up');
 var ansi = new AU.default;
 
-var disabled = true;
-var session = null;
+let socket = new Socket("/socket", {params: {token: window.userToken}});
+socket.connect();
+
+let channel = socket.channel("repl_session:lobby", {});
 
 let currentPrompt       = document.getElementById("prompt");
 let queryInput          = document.getElementById("query-input");
@@ -18,33 +20,9 @@ let submitButton        = document.getElementById("submit");
 let contractEditor      = document.getElementById("editor");
 let loadButton          = document.getElementById("load");
 
-disableInput();
-
-let socket = new Socket("/socket", {params: {token: window.userToken}});
-socket.connect();
-
-let channel = socket.channel("repl_session:lobby", {});
-
-function enableInput() {
-    queryInput.disabled = false;
-    newlineButton.disabled = false;
-    submitButton.disabled = false;
-    contractEditor.disabled = false;
-    loadButton.disabled = false;
-    disabled = false;
-}
-
-function disableInput() {
-    queryInput.disabled = true;
-    newlineButton.disabled = true;
-    submitButton.disabled = true;
-    contractEditor.disabled = true;
-    loadButton.disabled = true;
-    disabled = true;
-}
+var session = null;
 
 function handle_response(payload) {
-    console.log("Received response.");
     var msg = payload.msg;
     var last_prompt = currentPrompt.innerText;
     var prompt = payload.prompt ? payload.prompt : last_prompt;
@@ -59,8 +37,6 @@ function handle_response(payload) {
 }
 
 function submitQuery() {
-    if(disabled) return;
-
     let query = queryInput.value.trim();
     let prompt = currentPrompt.innerText + "> ";
 
@@ -69,17 +45,14 @@ function submitQuery() {
     messageItem.classList.add("in");
     outputContainer.appendChild(messageItem);
 
-    var t =channel.push("query", {input: query,
+    channel.push("query", {input: query,
                            user_session: session
                           })
-        .receive("ok", handle_response)
-        .receive("error", handle_response); // TODO why isn't this working?
+        .receive("ok", handle_response);
     queryInput.value = "";
 }
 
 function insertNewLine() {
-    if(disabled) return;
-
     let pos = queryInput.selectionStart;
     let input = queryInput.value;
     let left = input.substr(0, pos);
@@ -90,19 +63,12 @@ function insertNewLine() {
 }
 
 function loadFiles() {
-    if(disabled) return;
-
     let contract = contractEditor.value;
-    channel.push("update_files",
-                 {files: [{filename: "contract.aes",
-                           content: contract
-                          }],
-                  user_session: session
-                 });
-    channel.push("load",
-                 {files: ["contract.aes"],
-                  user_session: session
-                 });
+    channel.push("load", {files: [{filename: "contract.aes",
+                                   content: contract
+                                  }],
+                          user_session: session
+                         });
 }
 
 function log_response(msg) {
@@ -114,7 +80,7 @@ function log_response(msg) {
 }
 
 function update_prompt(prompt) {
-    let prompt_text = prompt;
+    let prompt_text = prompt + "> ";
     currentPrompt.innerText = prompt_text;
     queryInput.placeholder = prompt_text;
 }
@@ -129,29 +95,19 @@ queryInput.addEventListener("keypress", event => {
     }
 });
 
-channel.onError( (e) => {
-    console.log("Channel error:", e);
-    update_prompt("(ERROR)");
-    disableInput();
+channel.on("response", payload => {
+    handle_response(payload)
 });
+
+channel.onError( () => alert("Channel error.") );
 channel.onClose( () => {
-    console.log("Channel closed");
     update_prompt("(CLOSED)");
-    disableInput();
+    alert("The channel has been closed. Please refresh to start a new session.");
 });
 
 
 channel.join()
-    .receive("ok", resp => {
-        console.log("Joined aerepl lobby.");
-        session = resp.user_session;
-        console.log("Session: ", session);
-        var t = channel.push("banner", {user_session: session})
-            .receive("ok", handle_response);
-        console.log("Session established.");
-        enableInput();
-
-    })
+    .receive("ok", resp => { console.log("Joined aerepl lobby."); })
     .receive("error", resp => {
         update_prompt("(CHANNEL ERROR)");
         alert("Could not establish the connection.");
--- lib/aerepl_server_web/channels/repl_session_channel.ex
+++ lib/aerepl_server_web/channels/repl_session_channel.ex
@@ -3,11 +3,48 @@ defmodule AereplServerWeb.ReplSessionChannel do
   require Logger
 
   alias AereplServer.{
+    SessionData,
     SessionService,
   }
 
 
   def join("repl_session:lobby", payload, socket) do
+    send(self(), {:init, payload})
+    {:ok, socket}
+  end
+
+
+  def handle_in("query", %{"input" => input, "user_session" => client_id}, socket) do
+    IO.inspect("QUERY")
+    out = AereplServer.SessionService.repl_input_text(client_id, input)
+    prompt = AereplServer.SessionService.repl_prompt(client_id)
+
+    resp = %{"msg" => out, "prompt" => prompt}
+#    push(socket, "response", resp)
+
+    IO.inspect("REPLY " <> out)
+    {:reply, {:ok, resp}, socket}
+  end
+
+  def handle_in("load", %{"files" => files, "user_session" => client_id}, socket) do
+    filemap = for %{"filename" => filename,
+                    "content" => content} <- files,
+      do: {String.to_charlist(filename), content}
+
+    out = AereplServer.SessionService.repl_load_files(client_id, filemap)
+    prompt = AereplServer.SessionService.repl_prompt(client_id)
+
+    resp = %{"msg" => out, "prompt" => prompt}
+    push(socket, "response", resp)
+
+    {:noreply, socket}
+  end
+
+  def handle_in(t, p, socket) do
+    IO.inspect("WTF " <> t)
+  end
+
+  def handle_info({:init, payload}, socket) do
     client_id =
       case payload do
         %{"user_session" => token} ->
@@ -18,183 +55,12 @@ defmodule AereplServerWeb.ReplSessionChannel do
 
     SessionService.try_start(client_id)
 
-    resp = %{"user_session" => client_id}
-    {:ok, resp, socket}
-  end
-
-  def repl_call(client_id, data, socket) do
-    repl_call(client_id, data, socket, fn x -> x end)
-  end
-  def repl_call(client_id, data, socket, cont) do
-    # TODO: This should return raw data, not rendered.
-    # 1. Change opts return_mode to value
-    # 2. Add feature to call with different opts
-    output = SessionService.repl_call(client_id, data)
-    {:ok, prompt} = AereplServer.SessionService.repl_prompt(client_id)
-    resp = %{"msg" => cont.(output), "prompt" => prompt}
-    {:reply, {:ok, resp}, socket}
-  end
-
-  def repl_call_render(client_id, data, socket) do
-    output = SessionService.repl_call_render(client_id, data)
-    {:reply, {:ok, output}, socket}
-  end
-
-  def repl_cast(client_id, data, socket) do
-    SessionService.repl_call(client_id, data)
+    msg = SessionService.repl_banner(client_id)
+    prompt = AereplServer.SessionService.repl_prompt(client_id)
+    resp = %{"msg" => msg, "user_session" => client_id, "prompt" => prompt}
+    push(socket, "response", resp)
     {:noreply, socket}
-  end
-
-
-  def handle_in("query", %{"input" => input, "user_session" => client_id}, socket) do
-    output = AereplServer.SessionService.repl_input_text(client_id, input)
-
-    prompt =
-      case output do
-        :finish -> "Bye!"
-        _ ->
-          {:ok, p} = AereplServer.SessionService.repl_prompt(client_id)
-          p
-      end
-
-    case output do
-      msg when is_binary(msg) ->
-        {:reply, {:ok, %{"msg" => msg, "prompt" => prompt}}, socket}
-      {:ok, msg} ->
-        {:reply, {:ok, %{"msg" => msg, "prompt" => prompt}}, socket}
-      :ok ->
-        {:reply, {:ok, %{"msg" => "", "prompt" => prompt}}, socket}
-      :finish ->
-        {:stop, {:shutdown, :closed},
-         {:ok, %{"msg" => "bye!", "prompt" => prompt}}, socket}
-      {:error, msg} ->
-        # TODO This should not return :ok
-        {:reply, {:ok, %{"msg" => msg, "prompt" => prompt}}, socket}
-    end
-  end
-
-  def handle_in("update_files", %{"files" => files, "user_session" => client_id}, socket) do
-
-    filemap = for %{"filename" => filename, "content" => content} <- files, do:
-      {String.to_charlist(filename), content}
-
-    AereplServer.SessionService.repl_cast(
-      client_id,
-      {:update_filesystem_cache, filemap})
-
-    {:noreply, socket}
-  end
-
-  def handle_in("skip", %{"user_session" => client_id}, socket) do
-    repl_cast(client_id, :skip, socket)
-  end
-  def handle_in("reset", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :reset, socket)
-  end
-
-  def handle_in("type", %{"user_session" => client_id, "expr" => expr}, socket) do
-    repl_call(client_id, {:type, expr}, socket)
-  end
-
-  def handle_in("state", %{"user_session" => client_id, "expr" => expr}, socket) do
-    repl_call(client_id, {:state, expr}, socket)
-  end
-
-  def handle_in("eval", %{"user_session" => client_id, "expr" => expr}, socket) do
-    repl_call(client_id, {:eval, expr}, socket)
-  end
-
-  def handle_in("load", %{"user_session" => client_id, "files" => files}, socket) do
-    files = for file <- files, do: String.to_charlist(file)
-    repl_call(client_id, {:load, files}, socket)
-  end
-
-  def handle_in("reload", %{"user_session" => client_id, "files" => files}, socket) do
-    repl_call(client_id, {:reload, files}, socket)
-  end
-
-  def handle_in("update_filesystem_cache", %{"user_session" => client_id, "files" => files}, socket) do
-    repl_cast(client_id, {:update_filesystem_cache, files}, socket)
-  end
-
-  def handle_in("set", %{"user_session" => client_id, "option" => option, "value" => value}, socket) do
-    repl_call(client_id, {:set, option, value}, socket)
-  end
-
-  def handle_in("help", %{"user_session" => client_id, "command" => command}, socket) do
-    repl_call(client_id, {:help, command}, socket)
-  end
-
-  def handle_in("help", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :help, socket)
-  end
-
-  def handle_in("lookup", %{"user_session" => client_id, "what" => what}, socket) do
-    repl_call(client_id, {:lookup, what}, socket)
-  end
-
-  def handle_in("disas", %{"user_session" => client_id, "ref" => ref}, socket) do
-    repl_call(client_id, {:disas, ref}, socket)
-  end
-
-  def handle_in("break", %{"user_session" => client_id, "file" => file, "line" => line}, socket) do
-    repl_call(client_id, {:break, file, line}, socket)
-  end
-
-  def handle_in("delete_break", %{"user_session" => client_id, "id" => id}, socket) do
-    repl_call(client_id, {:delete_break, id}, socket)
-  end
-
-  def handle_in("delete_break_loc", %{"user_session" => client_id, "file" => file, "line" => line}, socket) do
-    repl_call(client_id, {:delete_break_loc, file, line}, socket)
-  end
-
-  def handle_in("continue", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :continue, socket)
-  end
-
-  def handle_in("stepover", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :stepover, socket)
-  end
-
-  def handle_in("stepin", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :stepin, socket)
-  end
-
-  def handle_in("stepout", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :stepout, socket)
-  end
-
-  def handle_in("location", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :location, socket)
-  end
-
-  def handle_in("print_var", %{"user_session" => client_id, "name" => name}, socket) do
-    repl_call(client_id, {:print_var, name}, socket)
-  end
-
-  def handle_in("print_vars", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :print_vars, socket)
-  end
-
-  def handle_in("stacktrace", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :stacktrace, socket)
-  end
-
-  def handle_in("version", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :version, socket)
-  end
-
-  def handle_in("banner", %{"user_session" => client_id}, socket) do
-    repl_call(client_id, :banner, socket)
-  end
-
-  def handle_in(t, p, socket) do
-    IO.inspect("Unknown message `" <> t <> "`:")
-    IO.inspect(p)
-    {:noreply, socket}
-  end
-
+end
 
   def new_client_id() do
     UUID.uuid4()
